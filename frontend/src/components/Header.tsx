1a0b6b6e6de68a7456c1ecbd63b1dfddeba24b91
Samuel k <skifle507@gmail.com>
2025-03-27 13:52:07 +0300

    add logo on header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Filter, PlusCircle, Settings } from "lucide-react";
 import { DarkMode } from "./DarkMode";
 import { Link, useNavigate } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Header = () => {
   const [search, setSearch] = useState("");
@@ -12,27 +13,29 @@ const Header = () => {
   return (
     <header className="flex flex-col justify-between items-center p-2 md:p-4 w-full">
       <div className="flex justify-between items-center px-3 w-full">
-        <div>
-          <h2 className="text-2xl font-bold">Welcome back!</h2>
-          <p className="text-gray-600">
-            Here's a list of your tasks for this month!
-          </p>
+        <div className="md:flex gap-4">
+          <Link
+            to="/"
+            className="text-3xl sm:text-4xl md:text-5xl font-extrabold tracking-tight text-primary cursor-pointer"
+          >
+            <span className="bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
+              TasKY
+            </span>
+          </Link>
+          <div>
+            <h2 className="text-2xl font-bold">Welcome back!</h2>
+            <p className="text-gray-200">
+              Here's a list of your tasks for you!
+            </p>
+          </div>
         </div>
-        <div>
-          <Button variant="outline" onClick={() => navigate("/login")}>
-            Login
-          </Button>
-          <Button variant="outline" onClick={() => navigate("/register")}>
-            Register
-          </Button>
-        </div>
-        <div className="rounded-full overflow-hidden w-10 h-10 bg-gray-200">
-          <img
+        <Avatar onClick={() => navigate("/profile")}>
+          <AvatarImage
             src="https://avatars.githubusercontent.com/u/123456?v=4"
             alt="avatar"
-            className="w-full h-full object-cover"
           />
-        </div>
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
       </div>
 
       <nav className="grid grid-cols-2 sm:grid-cols-3 lg:flex lg:items-center justify-between gap-2 mt-4 px-2 w-full">
@@ -46,27 +49,26 @@ const Header = () => {
             className="border p-2 rounded-md w-full"
           />
         </div>
-
-        {/* Buttons - Stack on mobile, inline on desktop */}
         <Button
           variant="outline"
-          className="flex items-center gap-1 w-full sm:w-auto"
+          className="flex items-center gap-1 w-full sm:w-auto bg-purple-500 hover:bg-purple-400 text-white"
         >
-          <PlusCircle size={16} /> Status
+          <Filter size={16} /> Status
         </Button>
 
         <Button
-          variant="outline"
-          className="flex items-center gap-1 w-full sm:w-auto"
+          variant={"outline"}
+          className="flex items-center gap-1 w-full sm:w-auto bg-purple-500 hover:bg-purple-400 text-white"
         >
-          <PlusCircle size={16} /> Priority
+          <Filter size={16} /> Priority
         </Button>
 
         <Link to="/create" className="w-full sm:w-auto">
           <Button
             variant="outline"
-            className="flex items-center gap-1 w-full sm:w-auto"
+            className="flex items-center gap-1 w-full sm:w-auto bg-purple-500 hover:bg-purple-400 text-white"
           >
+            <PlusCircle size={16} />
             New Task
           </Button>
         </Link>
@@ -89,3 +91,12 @@ const Header = () => {
 export default Header;
 
 //       <div className="flex flex-col gap-4 items-center space-x-4"></div>
+
+/* <div>
+          <Button variant="outline" onClick={() => navigate("/login")}>
+            Login
+          </Button>
+          <Button variant="outline" onClick={() => navigate("/register")}>
+            Register
+          </Button>
+        </div> */
