efb7c235191139ecf06a7bac41f0917c2624dc8b
Samuel k <skifle507@gmail.com>
2025-03-27 09:51:45 +0300

    add task edit, detail form

---
+++ frontend/src/pages/TaskDetailPage.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import { TaskEditForm } from "@/components/TaskEditForm";
+
+const TaskDetailPage = () => {
+  return (
+    <>
+      <Header />
+      <TaskEditForm />
+    </>
+  );
+};
+
+export default TaskDetailPage;
